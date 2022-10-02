65872a2d2297b49660b1df91f98f9b61f5891844
jks-sn <110144935+jks-sn@users.noreply.github.com>
2022-10-02 21:03:13 +0700

    Delete cmake-build-debug directory

--- cmake-build-debug/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/User/Documents/GitHub/Labs/googletest/googletest
-# Build directory: C:/Users/User/Documents/GitHub/Labs/cmake-build-debug/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
