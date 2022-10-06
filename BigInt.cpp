24ecf29c6f63cd6bdd1edbcc61055c8ee18c0a5b
kberkaev <maxotr@mail.ru>
2022-10-06 20:03:51 +0700

    Update BigInt.cpp

--- BigInt.cpp
+++ BigInt.cpp
@@ -14,7 +14,7 @@ BigInt::BigInt() : counter(1), sign(false), data(new unsigned int[this->counter]
     data[0] = 0;
 }
 
-BigInt::BigInt(int n) : counter(1), sign(false), data(new unsigned int[this->counter]) {
+BigInt::BigInt(int n) : counter(1), sign(n<0), data(new unsigned int[this->counter]) {
     data[0] = abs(n);
     if (n < 0)
         data[0] = ~(data[0])+1;
