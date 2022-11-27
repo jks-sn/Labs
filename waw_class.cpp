eb7aab57c9755353430c5d39474c9d2672e00e74
kberkaev <maxotr@mail.ru>
2022-11-28 00:01:41 +0700

    1

---
+++ waw_class.cpp
@@ -0,0 +1,5 @@
+//
+// Created by User on 27.11.2022.
+//
+
+#include "waw_class.h"
---
+++ waw_class.h
@@ -0,0 +1,11 @@
+//
+// Created by User on 27.11.2022.
+//
+#define FREQ 44100
+#ifndef LABS_WAW_CLASS_H
+#define LABS_WAW_CLASS_H
+class Waw
+{
+    
+};
+#endif //LABS_WAW_CLASS_H
